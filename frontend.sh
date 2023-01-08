f65c7635ee5348d9f5a6a54ee8a57fac37948f44
nagababu <nag4c1@gmail.com>
2023-01-08 14:25:18 +0530

    configured frontend microservice

--- frontend.sh
+++ frontend.sh
@@ -1,21 +1,28 @@
 script_location=$(pwd)
 
-echo -e '\e[31m install nginx\e[0m'
+echo -e '\e[32m install nginx\e[0m'
 yum install nginx -y
 
+echo -e '\e[32m enable nginx\e[0m'
 systemctl enable nginx
 
+echo -e '\e[32m start nginx\e[0m'
 systemctl start nginx
 
+echo -e '\e[32m install nginx\e[0m'
 rm -rf /usr/share/nginx/html/*
 
+echo -e '\e[32m download frontend content\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
 
 cd /usr/share/nginx/html
 
+echo -e '\e[32m Unzipping the frontend content\e[0m'
 unzip /tmp/frontend.zip
 
+echo -e '\e[32m Coping reverse proxy conf \e[0m'
 cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf
 
+echo -e '\e[32m restarting nginx\e[0m'
 systemctl restart nginx
 
