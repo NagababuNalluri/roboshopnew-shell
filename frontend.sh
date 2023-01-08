0f33a170a1b00959b800d54fa9369b1553ff6148
nagababu <nag4c1@gmail.com>
2023-01-08 21:03:28 +0530

    configured frontend microservice

--- frontend.sh
+++ frontend.sh
@@ -1,28 +1,28 @@
 script_location=$(pwd)
-
+log=/tmp/roboshop.logs
 echo -e '\e[32m install nginx\e[0m'
-yum install nginx -y
+yum install nginx -y &>>${log}
 
 echo -e '\e[32m enable nginx\e[0m'
-systemctl enable nginx
+systemctl enable nginx &>>${log}
 
 echo -e '\e[32m start nginx\e[0m'
-systemctl start nginx
+systemctl start nginx &>>${log}
 
 echo -e '\e[32m install nginx\e[0m'
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>${log}
 
 echo -e '\e[32m download frontend content\e[0m'
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 
-cd /usr/share/nginx/html
+cd /usr/share/nginx/html &>>${log}
 
 echo -e '\e[32m Unzipping the frontend content\e[0m'
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>${log}
 
 echo -e '\e[32m Coping reverse proxy conf \e[0m'
-cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf
+cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf &>>${log}
 
 echo -e '\e[32m restarting nginx\e[0m'
-systemctl restart nginx
+systemctl restart nginx &>>${log}
 
