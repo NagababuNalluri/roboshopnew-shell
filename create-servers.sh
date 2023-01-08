8f1a29b9efe499e78c4e455f053b903455f6f399
nagababu <nag4c1@gmail.com>
2023-01-08 17:00:13 +0530

    configured frontend microservice

--- create-servers.sh
+++ create-servers.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 ##### Change these values ###
-ZONE_ID="Z0366464237Z7LZLZPKFA"
-DOMAIN="devopsb70.online"
+ZONE_ID="Z0684247D5O5IEDBEU86"
+DOMAIN="devopshemasri.online"
 SG_NAME="allow-all"
 env=dev
 #############################
