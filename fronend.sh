15a0232ac1f8cfc17a429482c8a983655a2f4c2d
nagababu <nag4c1@gmail.com>
2023-01-08 08:01:34 +0530

    configured frontend microservice

---
+++ fronend.sh
@@ -0,0 +1,20 @@
+script_location=$(pwd)
+
+yum install nginx -y
+
+systemctl enable nginx
+
+systemctl start nginx
+
+rm -rf /usr/share/nginx/html/*
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+cd /usr/share/nginx/html
+
+unzip /tmp/frontend.zip
+
+cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf
+
+systemctl restart nginx
+
