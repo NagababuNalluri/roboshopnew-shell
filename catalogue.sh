da79c6c712826b39e38caea8c3338bc19e9b6a46
nagababu <nag4c1@gmail.com>
2023-01-08 12:59:47 +0530

    configured frontend microservice

---
+++ catalogue.sh
@@ -0,0 +1,33 @@
+script_location=$(pwd)
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+yum install nodejs -y
+
+useradd roboshop
+
+mkdir /app mkdir /app
+
+
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+
+rm -rf /app
+
+cd /app
+
+unzip /tmp/catalogue.zip
+
+cd /app
+
+npm install
+
+cp '$script_location'/files/catalogue.service /etc/systemd/system/catalogue.service
+
+systemctl daemon-reload
+
+systemctl enable catalogue
+
+systemctl start catalogue
+
+
+
