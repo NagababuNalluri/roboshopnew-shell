9a4b8756c664bac6f9218a95233ffb86688fb2be
nagababu <nag4c1@gmail.com>
2023-01-09 09:30:20 +0530

    updating ./roboshopnew-shell

--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ echo -e '\e[32m install nginx\e[0m'
 yum install nginx -y &>>${log}
 if [ $? -eq 0 ]
 then
-  echo '\e[35m success \e[0m'
+  echo -e '\e[35m success \e[0m'
   fi
 
 echo -e '\e[32m enable nginx\e[0m'
