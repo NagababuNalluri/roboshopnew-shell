1eaaa5de838858822c3b82302129091c1a37c348
nagababu <nag4c1@gmail.com>
2023-01-09 16:10:28 +0530

    updating ./roboshopnew-shell

--- frontend.sh
+++ frontend.sh
@@ -2,6 +2,17 @@ script_location=$(pwd)
 
 log=/tmp/roboshop.logs
 
+check_status() {
+  if [ $? -eq 0 ]
+then
+  echo -e '\e[32m success\e[0m'
+  else
+    echo -e '\e[31m Fail\e[0m'
+    echo 'Refer log files for more info log-${log}'
+    exit
+    fi
+    }
+
 echo -e '\e[32m install nginx\e[0m'
 yum install nginx -y &>>${log}
 
