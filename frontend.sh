696f25544be404934a64ff82ae518bf920022f95
nagababu <nag4c1@gmail.com>
2023-01-09 09:27:19 +0530

    updating ./roboshopnew-shell

--- frontend.sh
+++ frontend.sh
@@ -16,21 +16,39 @@ then
 
 echo -e '\e[32m start nginx\e[0m'
 systemctl start nginx &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+  fi
 echo -e '\e[32m install nginx\e[0m'
 rm -rf /usr/share/nginx/html/* &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+  fi
 echo -e '\e[32m download frontend content\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+  fi
 cd /usr/share/nginx/html &>>${log}
 
 echo -e '\e[32m Unzipping the frontend content\e[0m'
 unzip /tmp/frontend.zip &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+  fi
 echo -e '\e[32m Coping reverse proxy conf \e[0m'
 cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+  fi
 echo -e '\e[32m restarting nginx\e[0m'
 systemctl restart nginx &>>${log}
-echo $?
+if [ $? -eq 0 ]
+then
+  echo success
+  fi
