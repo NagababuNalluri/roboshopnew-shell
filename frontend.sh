a71d5475acd61963a8d3a8b3c56e149b158997c2
nagababu <nag4c1@gmail.com>
2023-01-09 09:39:43 +0530

    updating ./roboshopnew-shell

--- frontend.sh
+++ frontend.sh
@@ -15,7 +15,9 @@ echo -e '\e[32m enable nginx\e[0m'
 systemctl enable nginx &>>${log}
 if [ $? -eq 0 ]
 then
-  echo -e '\e[35m success \e[0m'
+  echo -e '\e[35m Command Execution Success \e[0m'
+  else
+    echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 
 echo -e '\e[32m start nginx\e[0m'
@@ -28,13 +30,17 @@ echo -e '\e[32m install nginx\e[0m'
 rm -rf /usr/share/nginx/html/* &>>${log}
 if [ $? -eq 0 ]
 then
-  echo -e '\e[35m success \e[0m'
+  echo -e '\e[35m Command Execution Success \e[0m'
+  else
+    echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m download frontend content\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
 if [ $? -eq 0 ]
 then
-  echo -e '\e[35m success \e[0m'
+  echo -e '\e[35m Command Execution Success \e[0m'
+  else
+    echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 cd /usr/share/nginx/html &>>${log}
 
@@ -42,17 +48,23 @@ echo -e '\e[32m Unzipping the frontend content\e[0m'
 unzip /tmp/frontend.zip &>>${log}
 if [ $? -eq 0 ]
 then
-  echo -e '\e[35m success \e[0m'
+  echo -e '\e[35m Command Execution Success \e[0m'
+  else
+    echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m Coping reverse proxy conf \e[0m'
 cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf &>>${log}
 if [ $? -eq 0 ]
 then
-  echo -e '\e[35m success \e[0m'
+  echo -e '\e[35m Command Execution Success \e[0m'
+  else
+    echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m restarting nginx\e[0m'
 systemctl restart nginx &>>${log}
 if [ $? -eq 0 ]
 then
-  echo -e '\e[35m success \e[0m'
+  echo -e '\e[35m Command Execution Success \e[0m'
+  else
+    echo -e '\e[35m Command Execution Failed \e[0m'
   fi
