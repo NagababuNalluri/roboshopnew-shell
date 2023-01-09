7bc45450f52a76cd347a4583e57c94c424b12ab1
nagababu <nag4c1@gmail.com>
2023-01-09 16:12:58 +0530

    updating ./roboshopnew-shell

--- frontend.sh
+++ frontend.sh
@@ -5,9 +5,9 @@ log=/tmp/roboshop.logs
 check_status() {
   if [ $? -eq 0 ]
 then
-  echo -e '\e[32m success\e[0m'
+  echo -e '\e[33m success\e[0m'
   else
-    echo -e '\e[31m Fail\e[0m'
+    echo -e '\e[34m Fail\e[0m'
     echo 'Refer log files for more info log-${log}'
     exit
     fi
@@ -20,60 +20,25 @@ check_status
 
 echo -e '\e[32m enable nginx\e[0m'
 systemctl enable nginx &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-    echo -e '\e[31m Command Execution Failed \e[0m'
-  fi
+check_status
 
 echo -e '\e[32m start nginx\e[0m'
 systemctl start nginx &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-      echo -e '\e[31m Command Execution Failed \e[0m'
-    fi
+check_status
 echo -e '\e[32m install nginx\e[0m'
 rm -rf /usr/share/nginx/html/* &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-    echo -e '\e[31m Command Execution Failed \e[0m'
-  fi
+check_status
 echo -e '\e[32m download frontend content\e[0m'
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-    echo -e '\e[31m Command Execution Failed \e[0m'
-  fi
+check_status
 cd /usr/share/nginx/html &>>${log}
-
+check_status
 echo -e '\e[32m Unzipping the frontend content\e[0m'
 unzip /tmp/frontend.zip &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-    echo -e '\e[31m Command Execution Failed \e[0m'
-  fi
+check_status
 echo -e '\e[32m Coping reverse proxy conf \e[0m'
 cp $script_location/files/reverseproxy-roboshop.conf /etc/nginx/default.d/roboshohp.conf &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-    echo -e '\e[31m Command Execution Failed \e[0m'
-  fi
+check_status
 echo -e '\e[32m restarting nginx\e[0m'
 systemctl restart nginx &>>${log}
-if [ $? -eq 0 ]
-then
-  echo -e '\e[35m Command Execution Success \e[0m'
-  else
-    echo -e '\e[31m Command Execution Failed \e[0m'
-  fi
+check_status
