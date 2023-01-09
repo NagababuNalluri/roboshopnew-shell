7cec0e141aca5d0cd49e3df1398eeaf49dacc7d3
nagababu <nag4c1@gmail.com>
2023-01-09 09:46:22 +0530

    updating ./roboshopnew-shell

--- mongodb.sh
+++ mongodb.sh
@@ -1,7 +1,9 @@
 script_location=$(pwd)
 
+log=/tmp/roboshop.logs
+
 echo -e '\e[32m Setting mongodb repo\e[0m'
-cp "$script_location"/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp "$script_location"/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>$log
 if [ $? -eq 0 ]
 then
   echo -e '\e[35m Command Execution Success \e[0m'
@@ -10,7 +12,7 @@ then
   fi
 
 echo -e '\e[32m Installed mongodb\e[0m'
-yum install mongodb-org -y
+yum install mongodb-org -y &>>$log
 if [ $? -eq 0 ]
 then
   echo -e '\e[35m Command Execution Success \e[0m'
@@ -18,7 +20,7 @@ then
     echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m Enable Mongodb\e[0m'
-systemctl enable mongod
+systemctl enable mongod &>>$log
 if [ $? -eq 0 ]
 then
   echo -e '\e[35m Command Execution Success \e[0m'
@@ -26,7 +28,7 @@ then
     echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m Start Mongodb\e[0m'
-systemctl start mongod
+systemctl start mongod &>>$log
 if [ $? -eq 0 ]
 then
   echo -e '\e[35m Command Execution Success \e[0m'
@@ -34,7 +36,7 @@ then
     echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m Change listing address to 0.0.0.0\e[0m'
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf &>>$log
 if [ $? -eq 0 ]
 then
   echo -e '\e[35m Command Execution Success \e[0m'
@@ -42,7 +44,7 @@ then
     echo -e '\e[35m Command Execution Failed \e[0m'
   fi
 echo -e '\e[32m restarting Mongodb\e[0m'
-systemctl restart mongod
+systemctl restart mongod &>>$log
 if [ $? -eq 0 ]
 then
   echo -e '\e[35m Command Execution Success \e[0m'
