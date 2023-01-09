a63bf429d2cddb0bac6b0ee544b1b2e9f62cf885
nagababu <nag4c1@gmail.com>
2023-01-09 10:02:03 +0530

    updating ./roboshopnew-shell

--- catalogue.sh
+++ catalogue.sh
@@ -1,37 +1,38 @@
 script_location=$(pwd)
+log=/tmp/roboshop.logs
 echo -e '\e[32m Configuring nodejs \e[0m'
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log
 echo -e '\e[32m Install Node JS\e[0m'
-yum install nodejs -y
+yum install nodejs -y &>>log
 echo -e '\e[32m Add roboshop user\e[0m'
-useradd roboshop
+useradd roboshop &>>log
 echo -e '\e[32m Creating app directory\e[0m'
-mkdir -p /app
+mkdir -p /app &>>log
 
 echo -e '\e[32m Download Catalogue content\e[0m'
-curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+curl -L -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>log
 echo -e '\e[32m Remove default content in app directory\e[0m'
-rm -rf /app/*
+rm -rf /app/* &>>log
 
-cd /app
+cd /app &>>log
 
-unzip /tmp/catalogue.zip
+unzip /tmp/catalogue.zip &>>log
 
 echo -e '\e[32m NPM install\e[0m'
-npm install
+npm install &>>log
 echo -e '\e[32m Copy catalogue service\e[0m'
-cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service
+cp ${script_location}/files/catalogue.service /etc/systemd/system/catalogue.service &>>log
 echo -e '\e[32m Load catalogue service\e[0m'
-systemctl daemon-reload
+systemctl daemon-reload &>>log
 echo -e '\e[32m Enable Catalogue\e[0m'
-systemctl enable catalogue
+systemctl enable catalogue &>>log
 echo -e '\e[32m Start Catalogue\e[0m'
-systemctl start catalogue
+systemctl start catalogue &>>log
 echo -e '\e[32m Coping Mongodb Repo\e[0m'
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>log
 echo -e '\e[32m Install Mongodb\e[0m'
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>log
 echo -e '\e[32m Load Schema\e[0m'
-mongo --host mongodb-dev.devopshemasri.online </app/schema/catalogue.js
+mongo --host mongodb-dev.devopshemasri.online </app/schema/catalogue.js &>>log
 
 
