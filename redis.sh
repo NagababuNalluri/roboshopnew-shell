eda53b1337b5e5effa239bf25d1227b00c25832f
nagababu <nag4c1@gmail.com>
2023-01-10 10:52:47 +0530

    updating ./roboshopnew-shell

--- redis.sh
+++ redis.sh
@@ -12,12 +12,12 @@ yum install redis -y &>>$log
 check_status
 
 print_head '  Change listing address to 0.0.0.0  '
-sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf  /etc/redis/redis.conf&>>$log
+sed -i -e 's/127.0.0.1/0.0.0.0/' /etc/redis.conf &>>$log
 check_status
 print_head '  Enable redis  '
 systemctl enable redis &>>$log
 check_status
 
-print_head '  Restarting Redis  ' 
-systemctl start redis &>>$log
+print_head '  Restarting Redis  '
+systemctl restart redis &>>$log
 check_status
