e1331a92b82dc8c9f84011b113ddff8c0caebcf5
nagababu <nag4c1@gmail.com>
2023-01-10 12:13:04 +0530

    updating ./roboshopnew-shell

--- user.sh
+++ user.sh
@@ -1,12 +1,12 @@
 source common.sh
   print_head ' Configuring nodejs '
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
 check_status
   print_head ' Install Node JS'
-yum install nodejs -y &>>log
+yum install nodejs -y
 check_status
   print_head ' Add roboshop user'
-id roboshop &>>log
+id roboshop
 if [ $? -ne 0 ]
 then
   useradd roboshop
@@ -14,43 +14,45 @@ then
 check_status
 
   print_head ' Creating app directory'
-mkdir -p /app &>>log
+mkdir -p /app
+
 check_status
 
   print_head ' Download user content'
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>log
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
+
 check_status
   print_head ' Remove default content in app directory'
-rm -rf /app/* &>>log
+rm -rf /app/*
 check_status
 
 cd /app &>>log
 
-unzip /tmp/user.zip &>>log
+unzip /tmp/user.zip
 
   print_head ' NPM install'
-npm install &>>log
+npm install
 check_status
   print_head ' Copy user service'
-cp ${script_location}/files/user.service /etc/systemd/system/user.service &>>log
+cp ${script_location}/files/user.service /etc/systemd/system/user.service
 check_status
   print_head ' Load user service'
-systemctl daemon-reload &>>log
+systemctl daemon-reload
 check_status
   print_head ' Enable user'
-systemctl enable user &>>log
+systemctl enable user
 check_status
   print_head ' Start user'
-systemctl start user &>>log
+systemctl start user
 check_status
   print_head ' Coping Mongodb Repo'
-cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo &>>log
+cp ${script_location}/files/mongodb.repo /etc/yum.repos.d/mongodb.repo
 check_status
   print_head ' Install Mongodb'
-yum install mongodb-org-shell -y &>>log
+yum install mongodb-org-shell -y
 check_status
   print_head ' Load Schema'
-mongo --host mongodb-dev.devopshemasri.online </app/schema/user.js &>>log
+mongo --host mongodb-dev.devopshemasri.online </app/schema/user.js 
 check_status
 
 
